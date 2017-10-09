d3e7ca07f1f49e0ade3b762d95b5ce73f715eb84
MRLmic <missi.luna@gmail.com>
2017-10-09 16:18:35 -0400

    Update Fail Message Moved

--- assets/scripts/ui.js
+++ assets/scripts/ui.js
@@ -36,7 +36,9 @@ const onGetSuccess = function (data) {
 
     $('.update-song').on('click', function (event) {
       event.preventDefault()
-      $(this).closest('h4').append('<form class="update-a-song">Need to update something about your song? No problem! Enter the new information here.<div class="form-group"><input type="text" name="song[title]" id="new_title" placeholder="Title"><input type="text" name="song[artist]" id="new_artist" placeholder="Artist"><input type="checkbox" name="song[written_by]" id="original_check" value="true" checked><label for="chk_email_alerts">Song originally recorded by this artist?</label><input type="text" name="song[year]" id="new_year" placeholder="Year Recorded"><button type="submit" class="btn btn-default">Update Song</button></div></form>')
+      $('.update-song').hide()
+      $('.delete-song').hide()
+      $(this).closest('h4').append('<form class="update-a-song"><div class="form-message">Enter the new information here. All fields are required.</div><div class="form-group"><input type="text" name="song[title]" id="new_title" placeholder="Title"><input type="text" name="song[artist]" id="new_artist" placeholder="Artist"><input type="checkbox" name="song[written_by]" id="original_check" value="true" checked><label for="chk_email_alerts">Song originally recorded by this artist?</label><input type="text" name="song[year]" id="new_year" placeholder="Year Recorded"><button type="submit" class="btn btn-default">Update Song</button></div></form><button type="submit" class="btn btn-default" id="cancel-update">Cancel</button>')
       $(".update-a-song").on('submit', function (event) {
         event.preventDefault()
         $('.prompt-div').text('Song updated.')
@@ -45,6 +47,10 @@ const onGetSuccess = function (data) {
         userApi.patchSong(data, songId)
           .then(updateSuccess)
           .catch(coldCall)
+      })
+      $('#cancel-update').on('click', function (event) {
+        userApi.get()
+          .then(onGetSuccess)
       })
     })
   } else {
@@ -65,7 +71,9 @@ const deleteSongSuccess = function () {
 const deleteSongFailure = function () {
   $('.prompt-div').text('Action failed.')
 }
-const coldCall = function () { $('.prompt-div').text('Action failed. You can only update your own songs, you sneaky 🐍') }
+const coldCall = function () {
+  $('.form-message').text('Edit Failed. All fields are required.')
+}
 
 // auth UI
 const onSignUpSuccess = function (data) {
